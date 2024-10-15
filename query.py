ff9bc9dc4a82732a68ef5c0a4284d158991a58e6
ZiXuan_Zhang <871523104@qq.com>
2024-10-15 14:48:54 +0800

    todo:rrget

--- query.py
+++ query.py
@@ -1,18 +1,20 @@
 # 参数化建模（计算结果查询）
 import tkinter
 from tkinter import ttk
+import tkinter.messagebox
+from data import rhdata
 
 rh_dict = {'压力':('0.1MPa', '0.3MPa', '0.5MPa'),
            '转速':('6630rpm', '8565rpm', '9210rpm'),
-           '节流环间隙':('0.1mm', '0.2mm', '0.3mm'),
-           '喷淋管径':('0.5mm', '0.6mm', '0.7mm'),
            '织构':('无织构', '激光脸'),
-           '喷淋角度':('35°', '38°', '41°'),
-           '喷淋管数':('2', '3')}
+           '节流环间隙':('无节流环', '0.1mm', '0.2mm', '0.3mm'),
+           '喷淋管径':('0.5mm', '0.6mm', '0.7mm'),
+           '喷淋角度':('38°', '35°', '41°'),
+           '喷淋管数':('1', '2')}
 rr_dict = {'压力':('0.1MPa', '0.2MPa', '0.3MPa'),
            '转速':('6630rpm', '8565rpm', '9210rpm'),
-           '出口直径':('2mm', '3mm'),
-           '织构':('无织构', '人字槽', '螺旋槽')}
+           '织构':('无织构', '人字槽', '螺旋槽'),
+           '出口直径':('2mm', '3mm')}
 jh_dict = {'转速':('18835rpm', '21255rpm', '22600rpm', '23950rpm', '25560rpm', '26906rpm'),
            '压力':('0.25MPa', '0.3MPa', '0.35MPa'),
            '入口管径':('0.7mm', '0.8mm'),
@@ -24,12 +26,29 @@ jr_dict = {'转速':('18835rpm', '21255rpm', '22600rpm', '23950rpm', '25560rpm',
            '进口管径':('1mm', '1.5mm')}
 
 def rhget_result():
-    result_dict=[]
+    result_dict=()
     i=1
     for key in rh_dict.keys():
-        result_dict.append(rh_dict[key].get())
+        result_dict = result_dict + (rh_dict[key].get(), )
         i = i+1
-    print(result_dict)
+    id = rhdata.get(result_dict, 'id not found')
+    if id == 'id not found':
+        tkinter.messagebox.showwarning('提示', '您所选择的组合不存在。\n请重新输入。')
+    else:
+        result_window = tkinter.Toplevel()
+        result_window.title('查询结果')
+        result_window.geometry('1600x600')
+        label1 = tkinter.Label(result_window, text='流域流动截面云图', font=('黑体', 20))
+        label1.grid(row=0, column=0)
+        result1 = tkinter.PhotoImage(file=f'source\\img\\1.{id}.1.png')
+        result1_label = tkinter.Label(result_window, image=result1)
+        result1_label.grid(row=1, column=0)
+        label2 = tkinter.Label(result_window, text='密封副温度分布云图', font=('黑体', 20))
+        label2.grid(row=0, column=1)
+        result2 = tkinter.PhotoImage(file=f'source\\img\\1.{id}.2.png')
+        result2_label = tkinter.Label(result_window, image=result2)
+        result2_label.grid(row=1, column=1)
+        result_window.mainloop()
     
 def rrget_result():
     result_dict=[]
@@ -65,13 +84,26 @@ def query_window():
     title_label = tkinter.Label(master, image=QueryTitle)
     title_label.grid(row=0, column=0, columnspan=4)
 
-    frame1 = tkinter.Frame(master, bd=3, relief='sunken', height=200)
+    
+    frame1 = tkinter.Frame(master, bd=6, bg='#DAE3F3',relief='groove', height=350, width=230,
+                           highlightcolor='red',
+                           highlightthickness=2)
+    frame1.grid_propagate(0)
     frame1.grid(row=1, column=0)
-    frame2 = tkinter.Frame(master, bd=3, relief='sunken', height=200)
+    frame2 = tkinter.Frame(master, bd=6, bg='#DAE3F3',relief='groove', height=350, width=230,
+                           highlightcolor='red',
+                           highlightthickness=2)
+    frame2.grid_propagate(0)
     frame2.grid(row=1, column=1)
-    frame3 = tkinter.Frame(master, bd=3, relief='sunken', height=200)
+    frame3 = tkinter.Frame(master, bd=6, bg='#DAE3F3',relief='groove', height=350, width=230,
+                           highlightcolor='red',
+                           highlightthickness=2)
+    frame3.grid_propagate(0)
     frame3.grid(row=1, column=2)
-    frame4 = tkinter.Frame(master, bd=3, relief='sunken', height=200)
+    frame4 = tkinter.Frame(master, bd=6, bg='#DAE3F3',relief='groove', height=350, width=230,
+                           highlightcolor='red',
+                           highlightthickness=2)
+    frame4.grid_propagate(0)
     frame4.grid(row=1, column=3)
     
     frame = [frame1, frame2, frame3, frame4]
@@ -82,18 +114,24 @@ def query_window():
     
     # 创建label和combobox
     for i in range(4):
-        title_label = tkinter.Label(frame[i], text=title[i], font=('黑体',14))
-        title_label.grid(row=0, column=0, pady=10)
+        title_label = tkinter.Label(frame[i], text=title[i], font=('黑体',18,'bold'), bg='#DAE3F3',
+                                    foreground='#0a1220')
+        title_label.grid(row=0, column=0, columnspan=2)
         j=1
         for key, value in ddict[i].items():
-            label = tkinter.Label(frame[i], text=key, justify='left', anchor='w', width=10)
-            label.grid(row=j, column=0, pady=5)
+            label = tkinter.Label(frame[i], text=key, pady=6, bg='#DAE3F3')
+            label.grid(row=j, column=0)
             ddict[i][key] = ttk.Combobox(frame[i], width=10)
             ddict[i][key]['values'] = value
             ddict[i][key].current(0)
             ddict[i][key].grid(row=j, column=1)
             j = j+1
-        run_button = tkinter.Button(frame[i], text=f'{title[i]}\n获取结果', command=func[i])
+        run_button = tkinter.Button(frame[i], text=f'{title[i]}\n获取结果', font=('宋体',14,'bold'), 
+                                    bg='#acafc9', command=func[i])
         run_button.grid(row=j, column=0, columnspan=2)
         
+    QueryBottom = tkinter.PhotoImage(file='source\\img\\bottomQuery.png')
+    bottom_label = tkinter.Label(master, image=QueryBottom)
+    bottom_label.grid(row=2, column=0, columnspan=4)
+    
     master.mainloop()
